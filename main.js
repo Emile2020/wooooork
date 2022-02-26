63d8fa72a83936b937281c344d4717c683566db1
Emile2020 <emiledebruyckere@gmail.com>
2022-02-26 11:56:50 +0100

    v

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-<iframe frameborder="0" height="95%" src="http://ult1.falix.gg:36695/#/" width="100%"></iframe>
+<meta http-equiv=”Content-Security-Policy” content=”upgrade-insecure-requests”>
+<iframe frameborder="0" height="95%" src="http://ult1.falix.gg:36695/" width="100%"></iframe>
 `
